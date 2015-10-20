c087dcb26d8e3aec9d2e892a9236519dc5499fcb
Petra Mille <mille.petra@gmail.com>
2015-10-20 15:45:35 +0200

    Started with DatabaseCommunication

--- AWA_Quiz/DatabaseCommunication.cs
+++ AWA_Quiz/DatabaseCommunication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,44 @@ namespace AWA_Quiz
 {
     public class DatabaseCommunication
     {
+        SqlConnection myConnection = new SqlConnection();
+        SqlCommand myCommand = new SqlCommand();
+
+        string connectionString;
+
+        public void FindStatistics()
+        {
+            try
+            {
+                myCommand.Connection = myConnection;
+                myConnection.ConnectionString = connectionString; // @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Contacts;Integrated Security=SSPI";
+                myConnection.Open();
+
+                //Call sp
+                myCommand.CommandText = "";
+                myCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                myCommand.Parameters.Clear();
+
+                //Write parameters to add
+                myCommand.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 12);
+                
+
+
+                myCommand.Parameters["@Username"].Value = "";
+                
+                
+                myCommand.Parameters["@"].Direction = System.Data.ParameterDirection.Output;
+
+                myCommand.ExecuteNonQuery();
+
+                myConnection.Close();
+                
+            }
+            catch (Exception)
+            {
+               
+                
+            }
+        }
     }
 }
