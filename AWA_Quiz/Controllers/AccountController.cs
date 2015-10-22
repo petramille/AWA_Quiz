4ea0cf118a14b67ac5c039636f74f494ccf014cf
Hedvig Schmocker <hedvig.schmocker@gmail.com>
2015-10-22 15:21:58 +0200

    Added quiz index as main home page

--- AWA_Quiz/Controllers/AccountController.cs
+++ AWA_Quiz/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace AWA_Quiz.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Quiz");
                 }
                 AddErrors(result);
             }
@@ -449,7 +449,7 @@ namespace AWA_Quiz.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Quiz");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
